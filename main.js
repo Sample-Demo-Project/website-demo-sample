e9faf29471c78a62dd294082034832c779be3ebc
Reena Tsai <tsaireena@gmail.com>
2026-01-17 02:02:48 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const container = document.getElementById("sample-list");
+
+for (let i = 216; i <= 223; i++) {
+    const section = document.createElement("section");
+    section.className = "card";
+
+    section.innerHTML = `
+        <h2>Sample ${i}</h2>
+        <a href="samples/sample-${i}/index.html">View Sample</a>
+    `;
+
+    container.appendChild(section);
+}
